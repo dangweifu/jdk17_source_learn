dbbae46f389d8d107060030b998a72830e92d77d
dangweifu <m13886933623@163.com>
2022-10-08 14:05:08 +0800

    feat:添加ignore文件和 README.md 文件

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author : dangweifu
+ * @version 1.0
+ * @date ${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}

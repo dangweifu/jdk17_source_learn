2306642ef691ba9837d01488ca55312fb8021ab7
dangweifu <m13886933623@163.com>
2022-10-08 16:03:12 +0800

    feat:添加jdk源码

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        List<Integer> r = list.stream().sorted(Comparator.comparing(Integer::intValue).reversed()).collect(Collectors.toList());
+        System.out.println(r);
     }
 }
